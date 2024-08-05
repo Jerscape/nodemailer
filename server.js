60d2d853463e0ec20a6cebf201c7215f8e19b4db
@jerscape <jeremydutton5@gmail.com>
2024-08-05 19:49:40 -0400

    added expressed server and basic contact form

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.static('public'));
+
+//res is an instance of the server response class (express) http.ServerResponse
+app.get('/', (req, res) =>{
+  res.send('hello!!')
+}) 
+
+app.listen(PORT, ()=>{
+  console.log(`Sever running on a port ${PORT}`)
+})
+
